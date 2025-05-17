d7beafb3ef3f79cdb234a277b996f74392e5e99f
erika0915 <soohee2001@gmail.com>
2025-05-18 04:36:20 +0900

    Feat : 1D-CNN 모델 구조 정의

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect 
 from fastapi.middleware.cors import CORSMiddleware
-import time
-import asyncio 
-import json
-import numpy as np
+import asyncio, json, numpy as np
+import sys, os
+
+# AI 모델 디렉토리 경로 추가
+sys.path.append(os.path.join(os.path.dirname(__file__), "ai_model"))
+
+from predict import load_model, predict_from_keypoints
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message" : "Hello FastAPI"}
+# 서버 시작 시 모델 한 번 로드 
+model = load_model()
 
-### 모델 연결 
 # CORS 설정 (프론트앤드에서 접근 허용)
 app.add_middleware(
     CORSMiddleware,
@@ -21,31 +22,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello FastAPI"}
+
 @app.websocket("/ws")
-async def websocket_endpoint(websocket:WebSocket):
+async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     print("WebSocket 연결됨")
 
     try:
         while True:
-            # 프론트에서 JSON 메시지 수신 
             data = await websocket.receive_text()
-            print("받은 메시지: ",data)
+            print("받은 메시지: ", data)
             message = json.loads(data)
             coordinates = message.get("coordinates", [])
-            print("좌표 수:", len(coordinates)) 
+            print("좌표 수:", len(coordinates))
 
-            # 좌표 수 확인 
             if len(coordinates) == 154:
-                keypoints = np.array(coordinates).reshape(1, 154)
-
-                # 여기서 수어 예측 모델 호출 
-                predicted_text = "아프다"
-
-                # 클라이언트에 응답 전송 
-                await websocket.send_text(json.dumps({"text" : predicted_text}))
+                keypoints = np.array([coordinates])
+                predicted_text = predict_from_keypoints(keypoints, model)
+                await websocket.send_text(json.dumps({"text": predicted_text}))
             else:
-                await websocket.send_text(json.dumps({"text" :  "좌표 개수 오류 : 154개가 아님"}))
-    
-    except WebSocketDisconnect: 
+                await websocket.send_text(json.dumps({"text": "좌표 개수 오류: 154개가 아님"}))
+
+    except WebSocketDisconnect:
         print("WebSocket 연결 종료")
